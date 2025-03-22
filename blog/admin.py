aceda6e8d658cbb44e84a571201efd01d6b24e77
alex-brokethecode <alexrtc.dev@gmail.com>
2025-03-21 22:42:31 -0500

    chore: Create views, urls and basic templates

--- blog/admin.py
+++ blog/admin.py
@@ -8,3 +8,31 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_created', 'date_updated',)
     list_filter = ('date_created', 'author__username', )
     search_fields = ('author', 'title',)
+
+
+"""
+Step 5: Create Views and URLs
+Implement views for:
+
+- List all posts (home page).
+- View a single post (detail page).
+- Create a post (only for logged-in users).
+- Update & delete a post (only by the post author).
+- Define corresponding URLs in urls.py.
+
+Step 6: Implement Templates
+
+- Use Django's template system to display the blog pages.
+- Create a base template for common UI elements (navbar, footer).
+- Style the pages using basic CSS (or Bootstrap).
+
+Step 7: User Authentication
+- Set up Django's authentication system for login, logout, and registration.
+- Restrict post creation/editing to logged-in users only.
+
+Step 8: Extra Features (Optional, but Recommended!)
+
+- Add pagination to the blog post list.
+- Implement a search feature.
+- Allow users to comment on posts.
+"""
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+
+def post_create(request):
+    return render(request, 'blog/post_create.html')
+
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    return render(request, 'blog/post_details.html', {'post': post})
+
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    return render(request, 'blog/blog_update.html', {'post': post})
+
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    return render(request, 'blog/blog_delete.html', {'post': post})
